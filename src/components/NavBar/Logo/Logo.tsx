6ca65da7a8216e0b887142a152d39b070df2453c
Ewa Zgorzelska <ewa.zgorzelska@gmail.com>
2023-01-10 15:18:22 +0100

    SHAR-2 fix logo link

--- src/components/NavBar/Logo/Logo.tsx
+++ src/components/NavBar/Logo/Logo.tsx
@@ -1,21 +1,20 @@
-import { Anchor, Image } from '@mantine/core';
+import { Link } from 'react-router-dom';
+import { Image } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
 
 const Logo = () => {
   const mediumScreen = useMediaQuery('(min-width: 767px)');
   return (
-    <Anchor
-      href='/'
-      h={mediumScreen ? '200px' : '150px'}
-      w={mediumScreen ? '200px' : '150px'}
-    >
+    <Link to='/'>
       <Image
+        height={mediumScreen ? '200px' : '150px'}
+        width={mediumScreen ? '200px' : '150px'}
         src={
           mediumScreen ? 'assets/logo-primary.png' : 'assets/logo-mobile.png'
         }
         alt='logo'
       />
-    </Anchor>
+    </Link>
   );
 };
 
